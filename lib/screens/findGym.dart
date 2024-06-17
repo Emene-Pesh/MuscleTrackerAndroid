7bd9d62a10d7bac6d4a9d8721e31b10ce9c66431
Emene-Pesh <emeneche@gmail.com>
2024-06-17 13:36:22 +0200

    Get Device Permission for location

--- lib/screens/findGym.dart
+++ lib/screens/findGym.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
@@ -13,11 +14,42 @@ class FindGymScreen extends StatefulWidget {
 class _FindGymScreenState extends State<FindGymScreen> {
   List<Map<String, dynamic>> _locations = [];
   List<Map<String, dynamic>> _FindGym = [];
+  LocationPermission permission = LocationPermission.denied;
+  String denied = '';
 
   @override
   void initState() {
     super.initState();
     _fetchAndComputeFindGym();
+  }
+
+  Future<void> _getLocationPermission() async {
+    bool serviceEnabled;
+    
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      setState(() {
+        ;
+      });
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        setState(() {
+          denied = 'Location permissions are denied';
+        });
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      setState(() {
+        denied = 'Location permissions are permanently denied, we cannot request permissions.';
+      });
+    }
   }
 
   Future<void> _fetchAndComputeFindGym() async {
@@ -29,45 +61,58 @@ class _FindGymScreenState extends State<FindGymScreen> {
       print('Locations: $locations');
 
       // Get current location
-      Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.high);
-      double currentLat = position.latitude;
-      double currentLon = position.longitude;
-      print('Current location: $currentLat, $currentLon');
-
-      // Calculate distances
-      for (var location in locations) {
-        print('Location: $location');
-        double distance = _calculateDistance(
-          currentLat,
-          currentLon,
-          location['Latitude'],
-          location['Longitude'],
-        );
-        location['distance'] = distance;
+      await _getLocationPermission();
+      double _calculateDistance(
+        double lat1, double lon1, double lat2, double lon2) {
+          const p = 0.017453292519943295; // Pi/180
+          final a = 0.5 -
+              cos((lat2 - lat1) * p) / 2 +
+              cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2;
+              return 12742 * asin(sqrt(a)); // 2 * R; R = 6371 km
+          };
+      // if (permission == LocationPermission.deniedForever) {
+      //   _FindGym=[{'Address': 'Could not get location', 'distance': denied}];
         
+      // }else if (permission == LocationPermission.denied) {
+        
+      //   
+      // }
+      // else 
+      print('Permission: $permission');
+      if (permission == LocationPermission.whileInUse || permission == LocationPermission.always) {
+        Position position = await Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.high);
+        double currentLat = position.latitude;
+        double currentLon = position.longitude;
+        print('Current location: $currentLat, $currentLon');
+
+        // Calculate distances
+        for (var location in locations) {
+          print('Location: $location');
+          double distance = _calculateDistance(
+            currentLat,
+            currentLon,
+            location['Latitude'],
+            location['Longitude'],
+          );
+          location['distance'] = distance;
+          
+        }
+        print('GRRR $locations');
+
+        // Sort by distance and get top 3
+        locations.sort((a, b) => a['distance'].compareTo(b['distance']));
+        setState(() {
+          _locations = locations;
+          _FindGym = locations.take(3).toList();
+        });
+      } else {
+        _FindGym=[{'Address': 'Could not get location. Check Permissions', 'distance': 0.0}];
       }
-      print('GRRR $locations');
-
-      // Sort by distance and get top 3
-      locations.sort((a, b) => a['distance'].compareTo(b['distance']));
-      setState(() {
-        _locations = locations;
-        _FindGym = locations.take(3).toList();
-      });
-    } else {
-      throw Exception('Failed to load locations');
-    }
+    };
   }
 
-  double _calculateDistance(
-      double lat1, double lon1, double lat2, double lon2) {
-    const p = 0.017453292519943295; // Pi/180
-    final a = 0.5 -
-        cos((lat2 - lat1) * p) / 2 +
-        cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2;
-    return 12742 * asin(sqrt(a)); // 2 * R; R = 6371 km
-  }
+  
 
   @override
   Widget build(BuildContext context) {
